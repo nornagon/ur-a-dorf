3a3cd01ed04363a31228f47b8151a4dad6aa7e1b
Jeremy Apthorp <nornagon@nornagon.net>
2019-12-03 20:08:12 -0800

    refactor

---
+++ df.js
@@ -0,0 +1,169 @@
+const net = require('net')
+const debug = require('debug')('dfhack-rpc')
+const protobuf = require('protobufjs')
+
+const {dfproto, RemoteFortressReader} = protobuf.loadSync([
+  'proto/CoreProtocol.proto',
+  'proto/Basic.proto',
+  'proto/BasicApi.proto',
+  'proto/RemoteFortressReader.proto',
+]).nested
+
+const methods = {
+  GetVersion: {in: dfproto.EmptyMessage, out: dfproto.StringMessage},
+  GetDFVersion: {in: dfproto.EmptyMessage, out: dfproto.StringMessage},
+  GetWorldInfo: {in: dfproto.EmptyMessage, out: dfproto.GetWorldInfoOut},
+  ListEnums: {in: dfproto.EmptyMessage, out: dfproto.ListEnumsOut},
+  ListUnits: {in: dfproto.ListUnitsIn, out: dfproto.ListUnitsOut},
+  SetUnitLabors: {in: dfproto.SetUnitLaborsIn, out: dfproto.EmptyMessage},
+
+  GetMapInfo: {plugin: 'RemoteFortressReader', in: dfproto.EmptyMessage, out: RemoteFortressReader.MapInfo},
+  GetBlockList: {plugin: 'RemoteFortressReader', in: RemoteFortressReader.BlockRequest, out: RemoteFortressReader.BlockList},
+  ResetMapHashes: {plugin: 'RemoteFortressReader', in: dfproto.EmptyMessage, out: dfproto.EmptyMessage},
+  GetCreatureRaws: {plugin: 'RemoteFortressReader', in: dfproto.EmptyMessage, out: RemoteFortressReader.CreatureRawList},
+}
+
+const makeReader = (s) => {
+  let pending = Buffer.alloc(0)
+  s.on('data', chunk => {
+    debug(`read socket: length=${chunk.length}`)
+    pending = Buffer.concat([pending, chunk])
+  })
+  return {
+    read: (size) => {
+      return new Promise((resolve, reject) => {
+        if (pending.length >= size) {
+          const ret = pending.slice(0, size)
+          pending = size === 0 ? Buffer.alloc(0) : pending.slice(size)
+          resolve(ret)
+          return
+        }
+        function ondata() {
+          if (pending.length >= size) {
+            const ret = pending.slice(0, size)
+            pending = size === 0 ? Buffer.alloc(0) : pending.slice(size)
+            s.off('data', ondata)
+            s.off('error', onerror)
+            resolve(ret)
+          }
+        }
+        function onerror(err) {
+          s.off('data', ondata)
+          s.off('error', onerror)
+          reject(err)
+        }
+        s.on('data', ondata)
+        s.on('error', onerror)
+      })
+    }
+  }
+}
+
+function qualifiedName(type) {
+  let name = type.name
+  let p = type
+  while ((p = p.parent) && p && p.name) {
+    name = `${p.name}.${name}`
+  }
+  return name
+}
+
+class DFConnection {
+  constructor() {
+    this._idTable = {}
+  }
+
+  async connect() {
+    this._socket = net.createConnection({
+      port: 5000
+    }, () => {
+      const header = Buffer.alloc(12)
+      header.write('DFHack?\n')
+      header.writeInt32LE(1, 8)
+      debug('handshake header:', header)
+      this._socket.write(header)
+    })
+    this._reader = makeReader(this._socket)
+    const response = await this._reader.read(12)
+    // TODO: validate response
+  }
+
+
+  _writeMessage(id, buf) {
+    const header = Buffer.alloc(8)
+    header.writeInt16LE(id)
+    // NB. dfhack message header struct isn't packed.
+    header.writeInt32LE(buf.length, 4)
+    debug('write header:', header)
+    this._socket.write(header)
+    if (buf.length > 0) {
+      debug('write buf:', buf)
+      this._socket.write(buf)
+    }
+  }
+
+  async _readMessage() {
+    const reader = this._reader
+    while (true) {
+      const header = await reader.read(8)
+      debug('read header:', header)
+      const id = header.readInt16LE(0)
+      // NB. dfhack message header struct isn't packed.
+      const size = header.readInt32LE(4)
+      debug(`           : id=${id} size=${size}`)
+      if (id === -2 /* RPC_REPLY_FAIL */) {
+        throw new Error(`failed: ${size}`)
+      }
+      const body = size > 0 ? await reader.read(size) : Buffer.alloc(0)
+      debug('read body:', body)
+      if (id === -1 /* RPC_REPLY_RESULT */) {
+        return body
+      } else if (id === -3 /* RPC_REPLY_TEXT */) {
+        debug('reply text:', body.toString())
+      }
+    }
+  }
+  async _invokeById(id, body) {
+    this._writeMessage(id, body)
+    return await this._readMessage()
+  }
+
+  async _bindMethod({method, inputMsg, outputMsg, plugin}) {
+    const obj = {method, inputMsg, outputMsg, plugin}
+    const err = dfproto.CoreBindRequest.verify(obj)
+    if (err)
+      throw new Error(err)
+    const message = dfproto.CoreBindRequest.create(obj)
+    const payload = dfproto.CoreBindRequest.encode(message).finish()
+    const replyPayload = await this._invokeById(0, payload)
+    const reply = dfproto.CoreBindReply.decode(replyPayload)
+    return reply.assignedId
+  }
+
+  async _invoke(method, params) {
+    if (!(method in methods)) {
+      throw new Error(`unknown method: '${method}'`)
+    }
+    const {in: inType, out: outType, plugin} = methods[method]
+
+    if (!(method in this._idTable)) {
+      debug('binding:', method)
+      const inputMsg = qualifiedName(inType)
+      const outputMsg = qualifiedName(outType)
+      const id = await this._bindMethod({method, inputMsg, outputMsg, plugin})
+      this._idTable[method] = id
+    }
+
+    debug('invoking:', method)
+    const requestPayload = inType.encode(inType.fromObject(params)).finish()
+    const responsePayload = await this._invokeById(this._idTable[method], requestPayload)
+    return outType.decode(responsePayload)
+  }
+
+  close() {
+    this._writeMessage(-4 /* RPC_REQUEST_QUIT */, Buffer.alloc(0))
+    this._socket.end()
+  }
+}
+
+module.exports = { DFConnection }
--- test.js
+++ test.js
@@ -4,159 +4,15 @@ const debug = require('debug')('dfhack-rpc')
 
 const protobuf = require('protobufjs')
 
-const makeReader = (s) => {
-  let pending = Buffer.alloc(0)
-  s.on('data', chunk => {
-    debug(`read socket: length=${chunk.length}`)
-    pending = Buffer.concat([pending, chunk])
-  })
-  return {
-    read: (size) => {
-      return new Promise((resolve, reject) => {
-        if (pending.length >= size) {
-          const ret = pending.slice(0, size)
-          pending = Buffer.from(pending.slice(size))
-          resolve(ret)
-          return
-        }
-        function ondata() {
-          if (pending.length >= size) {
-            const ret = pending.slice(0, size)
-            pending = Buffer.from(pending.slice(size))
-            s.off('data', ondata)
-            s.off('error', onerror)
-            resolve(ret)
-          }
-        }
-        function onerror(err) {
-          s.off('data', ondata)
-          s.off('error', onerror)
-          reject(err)
-        }
-        s.on('data', ondata)
-        s.on('error', onerror)
-      })
-    }
-  }
-}
-
-function qualifiedName(type) {
-  let name = type.name
-  let p = type
-  while ((p = p.parent) && p && p.name) {
-    name = `${p.name}.${name}`
-  }
-  return name
-}
+const { DFConnection } = require('./df')
 
 ;(async () => {
 
-const {dfproto, RemoteFortressReader} = (await protobuf.load([
-  'proto/CoreProtocol.proto',
-  'proto/Basic.proto',
-  'proto/BasicApi.proto',
-  'proto/RemoteFortressReader.proto',
-])).nested
+const df = new DFConnection()
 
-const s = net.createConnection({
-  port: 5000
-}, () => {
-  const header = Buffer.alloc(12)
-  header.write('DFHack?\n')
-  header.writeInt32LE(1, 8)
-  debug('handshake header:', header)
-  s.write(header)
-})
+await df.connect()
 
-const reader = makeReader(s)
-
-const response = await reader.read(12)
-
-function writeMessage(id, buf) {
-  const header = Buffer.alloc(8)
-  header.writeInt16LE(id)
-  // NB. dfhack struct isn't packed.
-  header.writeInt32LE(buf.length, 4)
-  debug('write header:', header)
-  s.write(header)
-  if (buf.length > 0) {
-    debug('write buf:', buf)
-    s.write(buf)
-  }
-}
-
-async function readMessage() {
-  while (true) {
-    const header = await reader.read(8)
-    debug('read header:', header)
-    const id = header.readInt16LE(0)
-    // NB. dfhack struct isn't packed.
-    const size = header.readInt32LE(4)
-    debug(`           : id=${id} size=${size}`)
-    if (id === -2 /* RPC_REPLY_FAIL */) {
-      throw new Error(`failed: ${size}`)
-    }
-    const body = size > 0 ? await reader.read(size) : Buffer.alloc(0)
-    debug('read body:', body)
-    if (id === -1 /* RPC_REPLY_RESULT */) {
-      return body
-    } else if (id === -3 /* RPC_REPLY_TEXT */) {
-      debug('reply text:', body.toString())
-    }
-  }
-}
-
-async function invokeById(id, body) {
-  writeMessage(id, body)
-  return await readMessage()
-}
-
-async function bindMethod({method, inputMsg, outputMsg, plugin}) {
-  const obj = {method, inputMsg, outputMsg, plugin}
-  const err = dfproto.CoreBindRequest.verify(obj)
-  if (err)
-    throw new Error(err)
-  const message = dfproto.CoreBindRequest.create(obj)
-  const payload = dfproto.CoreBindRequest.encode(message).finish()
-  const replyPayload = await invokeById(0, payload)
-  const reply = dfproto.CoreBindReply.decode(replyPayload)
-  return reply.assignedId
-}
-
-const methods = {
-  GetVersion: {in: dfproto.EmptyMessage, out: dfproto.StringMessage},
-  GetDFVersion: {in: dfproto.EmptyMessage, out: dfproto.StringMessage},
-  GetWorldInfo: {in: dfproto.EmptyMessage, out: dfproto.GetWorldInfoOut},
-  ListEnums: {in: dfproto.EmptyMessage, out: dfproto.ListEnumsOut},
-  ListUnits: {in: dfproto.ListUnitsIn, out: dfproto.ListUnitsOut},
-  SetUnitLabors: {in: dfproto.SetUnitLaborsIn, out: dfproto.EmptyMessage},
-
-  GetMapInfo: {plugin: 'RemoteFortressReader', in: dfproto.EmptyMessage, out: RemoteFortressReader.MapInfo},
-  GetBlockList: {plugin: 'RemoteFortressReader', in: RemoteFortressReader.BlockRequest, out: RemoteFortressReader.BlockList},
-  ResetMapHashes: {plugin: 'RemoteFortressReader', in: dfproto.EmptyMessage, out: dfproto.EmptyMessage},
-  GetCreatureRaws: {plugin: 'RemoteFortressReader', in: dfproto.EmptyMessage, out: RemoteFortressReader.CreatureRawList},
-}
-
-const idTable = {}
-async function invoke(method, params) {
-  if (!(method in methods)) {
-    throw new Error(`unknown method: '${method}'`)
-  }
-  const {in: inType, out: outType, plugin} = methods[method]
-
-  if (!(method in idTable)) {
-    debug('binding:', method)
-    const inputMsg = qualifiedName(inType)
-    const outputMsg = qualifiedName(outType)
-    const id = await bindMethod({method, inputMsg, outputMsg, plugin})
-    idTable[method] = id
-  }
-
-  debug('invoking:', method)
-  const requestPayload = inType.encode(inType.fromObject(params)).finish()
-  const responsePayload = await invokeById(idTable[method], requestPayload)
-  return outType.decode(responsePayload)
-}
+const invoke = df._invoke.bind(df)
 
 try {
   console.log(await invoke('GetVersion'))
@@ -201,8 +57,7 @@ try {
   */
 
 } finally {
-  writeMessage(-4, Buffer.alloc(0))
-  s.end()
+  df.close()
 }
 
 })().then(() => {
