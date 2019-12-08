a2cd2f5d157f621915743203931aa7d224545adc
Jeremy Apthorp <nornagon@nornagon.net>
2019-12-07 22:03:00 -0800

    simple cli tool for debugging

---
+++ call.js
@@ -0,0 +1,8 @@
+const util = require('util')
+const { DFConnection } = require('./df')
+const df = new DFConnection()
+const [, , method, arg] = process.argv
+df.connect().then(async () => {
+  console.log(util.inspect(await df[method](JSON.parse(arg || '{}')), {depth: Infinity, colors: true}))
+  df.close()
+})
