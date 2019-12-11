4b286a902e73eab9a97c78b0edc5ea1791c3fa75
Jeremy Apthorp <nornagon@nornagon.net>
2019-12-10 23:04:11 -0800

    simple localstorage db

--- datastore.js
+++ datastore.js
@@ -1,4 +1,8 @@
-const claims = []
+const { LocalStorage } = require('node-localstorage')
+
+const localStorage = new LocalStorage('./db')
+
+const claims = JSON.parse(localStorage.getItem('claims') || '[]')
 module.exports = {
   async getClaims(userId) {
     return claims.filter(claim => claim.userId === userId)
@@ -11,5 +15,6 @@ module.exports = {
       throw new Error(`Unit ${unitId} has already been claimed.`)
     }
     claims.push({userId, unitId})
+    localStorage.setItem('claims', JSON.stringify(claims))
   },
 }
